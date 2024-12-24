9f825b7a05065bc12fb77f0f6b8cd46b1006819b
Dmitrii Parfenov <parfenovds@protonmail.com>
2024-12-24 10:40:26 +0300

    init commit

---
+++ src/main/java/com/cleanwaters/cleanwatersmainapi/CleanWatersMainApiApplication.java
@@ -0,0 +1,13 @@
+package com.cleanwaters.cleanwatersmainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanWatersMainApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CleanWatersMainApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/cleanwaters/cleanwatersmainapi/CleanWatersMainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanwaters.cleanwatersmainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanWatersMainApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
